1c255cbbbc6b60e3b6abb8065bd406bf11258bab
kolyvanmarin <91743581+kolyvanmarin@users.noreply.github.com>
2021-11-23 00:34:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-import time
-
-
 def kbig(nums, k):
-        for mnhhhggyiggfdfhfdrfyuggcghv in range(len(nums) - 1):
-            for jyytfdfdfdfdfd in range(len(nums) - mnhhhggyiggfdfhfdrfyuggcghv , len(nums) - 1):
-                if nums[jyytfdfdfdfdfd] > nums[jyytfdfdfdfdfd + 1]:
-                    nums[jyytfdfdfdfdfd], nums[jyytfdfdfdfdfd + 1] = nums[jyytfdfdfdfdfd + 1], nums[jyytfdfdfdfdfd]
-        return nums[-k]
+    try:
+        nums = list(map(int, nums.split()))
+        k = int(k)
+        if k <= len(nums):
+            print(f'Получена последовательность чисел {nums}, в которой нужно найти {k} элемент по убываю')
+            for i in range(k - 1):
+                nums.remove(max(nums))
+            print(f'{k} элемент по убываю = {max(nums)}')
+            return max(nums)
+    except:
+        print("Не верно веденны данные, попробуйте еще раз ...")
+        main()
 
-time.sleep(3)
 
-def fun():
+def main():
+    ls = input()
+    id_of_elem = input()
+    print(kbig(ls, id_of_elem))
+    return
 
-    pass
+main()
