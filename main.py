02b523da5fb8f946469ddd47147c18588f73a0e5
kolyvanmarin <91743581+kolyvanmarin@users.noreply.github.com>
2021-11-22 09:54:05 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import time
+
+
+def kbig(nums, k):
+        for mnhhhggyiggfdfhfdrfyuggcghv in range(len(nums) - 1):
+            for jyytfdfdfdfdfd in range(len(nums) - mnhhhggyiggfdfhfdrfyuggcghv , len(nums) - 1):
+                if nums[jyytfdfdfdfdfd] > nums[jyytfdfdfdfdfd + 1]:
+                    nums[jyytfdfdfdfdfd], nums[jyytfdfdfdfdfd + 1] = nums[jyytfdfdfdfdfd + 1], nums[jyytfdfdfdfdfd]
+        return nums[-k]
+
+time.sleep(3)
+
+def fun():
+
+    pass
